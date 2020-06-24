434af26f862ff52f0bb926b1032bfa4edcbf101d
FIRST_NAME <shivendra.bd@gmail.com>
2020-06-24 23:47:58 +0530

    Added Basic Product Model and payment enum types

---
+++ OrderProcessingBL/Enums/AppEnums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessingBL.Enums
+{
+    class AppEnums
+    {
+        enum PaymentTypes
+        {
+            PhysicalProduct = 0,
+            Book = 1,
+            MemberShip = 2,
+            Upgrade = 3,
+            Video = 4
+        }
+
+    }
+}
---
+++ OrderProcessingBL/Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessingBL.Model
+{
+    class Product
+    {
+        public string Name  // property
+        { get; set; }
+        public string Description  // property
+        { get; set; }
+    }
+}
